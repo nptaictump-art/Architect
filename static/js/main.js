04f535f4a9a4aa80cfbd5d4195d673408143dc85
nptaictump-art <nptaictump@gmail.com>
2025-12-18 09:19:05 +0700

    Initial commit: static/js/main.js

---
+++ static/js/main.js
@@ -0,0 +1,10 @@
+// Main Logic for Interactions (Hybrid Approach)
+console.log("SciEquip Loaded");
+
+// Basic Modal Toggler
+function toggleModal(id) {
+    const el = document.getElementById(id);
+    if (el) el.classList.toggle('hidden');
+}
+
+// Add more vanilla JS interactivity here for tabs, etc.
